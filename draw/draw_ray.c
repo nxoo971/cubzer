ed3494908ab963a93f78e396a430056ecea7353c
rayan ferradi <ferradi.r@gmail.com>
2023-02-24 22:24:32 +0100

    norme en cours

--- draw/draw_ray.c
+++ draw/draw_ray.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_ray.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jewancti <jewancti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rferradi <rferradi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 00:35:35 by jewancti          #+#    #+#             */
-/*   Updated: 2023/02/24 21:31:07 by jewancti         ###   ########.fr       */
+/*   Updated: 2023/02/24 22:22:26 by rferradi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,15 +86,15 @@ double	dda(t_data *data, t_params *params, int *map_y, int *map_x)
 
 
 static
-void	end_textures(t_data *data, int x, int texture_pos, const int texture_x, const int colors[2])
+void	end_textures(t_data *data, int x, double texture_pos, const int texture_x, const int colors[2])
 {
 	int	y;
 	int	texture_y;
-	int	color;
 
 	y = -1;
 	while (++y < HEIGHT)
 	{
+		int	color;
 		if (y < data -> params.draw_start)
 			color = colors[0];
 		else if (y > data -> params.draw_end)
@@ -144,35 +144,6 @@ void	begin_textures(t_data *data, int x, double perp_wall_dist, int line_height)
 	texture_pos = (data -> params.draw_start - HEIGHT / 2 + line_height / 2) * data -> params.step;
 	end_textures(data, x, texture_pos, texture_x,
 					(int [2]){set_rgb(data -> map.color_ceil), set_rgb(data -> map.color_floor)});
-	for (int y = 0; y < HEIGHT; y++)
-	{
-		int color;
-		if (y < data -> params.draw_start)
-			color = set_rgb(data -> map.color_ceil);
-		else if (y > data -> params.draw_end)
-			color = set_rgb(data -> map.color_floor);
-		else
-		{
-			int texY = (int)texture_pos & (TEXTURE_HEIGHT - 1);
-			texture_pos += data -> params.step;
-			if (data -> params.side == 1)
-			{
-				if (data -> params.ray_y > 0)
-					color = data->texture[0][TEXTURE_HEIGHT * texY + texture_x];
-				else
-					color = data->texture[1][TEXTURE_HEIGHT * texY + texture_x];
-			}
-			else
-			{
-				if (data -> params.ray_x > 0)
-					color = data->texture[3][TEXTURE_HEIGHT * texY + texture_x];
-				else
-					color = data->texture[2][TEXTURE_HEIGHT * texY + texture_x];
-			}
-		}
-		data->buf[y][x] = color;
-		data -> params.re_buf = true;
-	}
 }
 
 static
@@ -184,6 +155,7 @@ void	loop(t_data *data)
 	int		p_x;
 	int		p_y;
 
+	printf("px = %i - py = %i\n", (int)data -> player.x, (int)data -> player.y);
 	for (int x = 0; x < WIDTH; x++)
 	{
 		camera_x = 2 * x / (double)WIDTH - 1;
@@ -207,77 +179,5 @@ void	loop(t_data *data)
 void	draw_gameplay(t_data *data)
 {
 	loop(data);
-	//const t_map	map = data->map;
-	//t_params	*params;
-	//t_player	*player;
-	//int			y;
-	//int			x;
-
-	//params = & data -> params;
-	//player = & data -> player;
-	////params -> re_buf = false;//???
-	//y = player->y;
-	//x = player->x;
-	//for (int x = 0; x < WIDTH; x++)
-	//{
-	//	double cameraX = 2 * x / (double)WIDTH - 1;
-	//	params -> ray_y = params -> dir_y + params -> plane_y * cameraX;
-	//	params -> ray_x = params -> dir_x + params -> plane_x * cameraX;
-
-	//	int mapX = (int)player->x;
-	//	int mapY = (int)player->y;
-	//	double perpWallDist = dda(data, params, &mapY, &mapX);
-
-	//	int lineHeight = (int)(HEIGHT / perpWallDist);
-	//	int drawStart = -lineHeight / 2 + HEIGHT / 2;
-	//	if (drawStart < 0)
-	//		drawStart = 0;
-	//	int drawEnd = lineHeight / 2 + HEIGHT / 2;
-	//	if (drawEnd >= HEIGHT)
-	//		drawEnd = HEIGHT - 1;
-
-	//double wallX;
-	//	if (data -> params.side == 0)
-	//		wallX = data->player.y + perp_wall_dist * data -> params.ray_y;
-	//	else
-	//		wallX = data->player.x + perp_wall_dist * data -> params.ray_x;
-	//	wallX -= floor((wallX));
-	//	int texX = (int)(wallX * (double)(TEXTURE_WIDTH));
-	//	if (data -> params.side == 0 && data -> params.ray_x > 0)
-	//		texX = TEXTURE_WIDTH - texX - 1;
-	//	if (data -> params.side == 1 && data -> params.ray_y < 0)
-	//		texX = TEXTURE_WIDTH - texX - 1;
-	//	double step = 1.0 * TEXTURE_HEIGHT / line_height;
-	//	double texPos = (data -> params.draw_start - HEIGHT / 2 + line_height / 2) * step;
-	//	for (int y = 0; y < HEIGHT; y++)
-	//	{
-	//		int color;
-	//		if (y < data -> params.draw_start)
-	//			color = set_rgb(data -> map.color_ceil);
-	//		else if (y > data -> params.draw_end)
-	//			color = set_rgb(data -> map.color_floor);
-	//		else
-	//		{
-	//			int texY = (int)texPos & (TEXTURE_HEIGHT - 1);
-	//			texPos += step;
-	//			if (data -> params.side == 1)
-	//			{
-	//				if (data -> params.ray_y > 0)
-	//					color = data->texture[0][TEXTURE_HEIGHT * texY + texX];
-	//				else
-	//					color = data->texture[1][TEXTURE_HEIGHT * texY + texX];
-	//			}
-	//			else
-	//			{
-	//				if (data -> params.ray_x > 0)
-	//					color = data->texture[3][TEXTURE_HEIGHT * texY + texX];
-	//				else
-	//					color = data->texture[2][TEXTURE_HEIGHT * texY + texX];
-	//			}
-	//		}
-	//		data->buf[y][x] = color;
-	//		data -> params.re_buf = true;
-	//	}
-
 	draw_buff(data);
 }
